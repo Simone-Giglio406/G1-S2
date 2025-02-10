c700c8ace671f8581c8f65f518cc76fd27f484f0
Simone-Giglio406 <gigliosimone.97@gmail.com>
2025-02-10 15:28:20 +0100

    added

--- script.js
+++ script.js
@@ -7,8 +7,31 @@ console.log(myName);
 let number1 = 12;
 let number2 = 20;
 
-console.log(number1 + number2);
+let result = number1 + number2;
 
+console.log(result);
+
+//esercizio 4
 let x = 12;
 
-let surname = "Giglio";
+console.log(x);
+
+//esercizio 5
+let myName = "Giglio";
+
+console.log(myName);
+
+//esercizo 6
+let number3 = 4;
+
+console.log(number3 - x);
+
+//esercizio 7
+let name1 = "john";
+let name2 = "John";
+
+const = 'name1 !== name2'
+
+console.log("name1 è diversa da name2");
+
+console.log("name1 è uguale a name2");
